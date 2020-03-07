b1ddba953a9277f41a5c2d9030e6f957b5cb5d52
dkd <dpawar128@gmail.com>
2020-03-07 17:51:35 +0530

    this is myntra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, Component } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { RouterModule, Routes } from "@angular/router";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { PagenotfoundComponent } from "./pagenotfound/pagenotfound.component";
+import { LoginComponent } from "./login/login.component";
+import { SearchComponent } from "./search/search.component";
+import { HomeComponent } from "./home/home.component";
+import { SignupComponent } from "./signup/signup.component";
+import { SalesComponent } from "./sales/sales.component";
+
+const Router: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "**", component: PagenotfoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PagenotfoundComponent,
+    LoginComponent,
+    SearchComponent,
+    HomeComponent,
+    SignupComponent,
+    SalesComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(Router)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
+})
+export class HomeComponent implements OnInit {
+  addImage = "../../assets/img/BUY.jpg";
+  addImage1 = "../../assets/img/Off60.jpg";
+  addImage2 = "../../assets/img/Off70.jpg";
+  addImage3 = "../../assets/img/Off80.jpg";
+  addImage4 = "../../assets/img/santa1.jpg";
+  addImage5 = "../../assets/img/santa2.jpg";
+  addImage6 = "../../assets/img/santa3.jpg";
+  addImage7 = "../../assets/img/santa4.jpg";
+  addImage8 = "../../assets/img/santa5.jpg";
+  addImage9 = "../../assets/img/santa6.jpg";
+  addImage10 = "../../assets/img/TopDeals.jpg";
+  addImage11 = "../../assets/img/winterWear.jpg";
+  addImage12 = "../../assets/img/FreeShoping.jpg";
+  SImg1 = "../../assets/img/Salman.jpg";
+  SImg2 = "../../assets/img/Style.jpg";
+  SImg3 = "../../assets/img/Time.jpg";
+
+  constructor() {}
+
+  ngOnInit() {}
+}
